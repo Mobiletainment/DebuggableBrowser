83ab0d4f9fad579325cbca54408d0981eafc5a9b
David Pertiller <david.pertiller@gmail.com>
2016-08-14 01:19:05 +0200

    version 1.2 with minor UI improvements

---
+++ app/src/main/java/net/pertiller/debuggablebrowser/MainActivity.java
@@ -0,0 +1,134 @@
+package net.pertiller.debuggablebrowser;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.os.Build;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    private WebView webView;
+    private EditText location;
+    private Button reload;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        webView = (WebView) findViewById(R.id.webView);
+        reload = (Button) findViewById(R.id.reload);
+        reload.setOnClickListener(new Button.OnClickListener(){
+            public void onClick(View v) {
+                openLocation();
+            }
+        });
+        location = (EditText) findViewById(R.id.url);
+        location.setImeActionLabel("Go", KeyEvent.KEYCODE_ENTER);
+        location.setFocusableInTouchMode(true);
+        location.requestFocus();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            WebView.setWebContentsDebuggingEnabled(true);
+        }
+
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setAllowUniversalAccessFromFileURLs(true);
+        settings.setAllowFileAccess(true);
+        settings.setAllowUniversalAccessFromFileURLs(true);
+        settings.setDomStorageEnabled(true);
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                super.onPageStarted(view, url, favicon);
+                location.setText(url);
+            }
+        });
+
+        webView.setWebChromeClient(new WebChromeClient());
+
+        location.setOnEditorActionListener(new EditText.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_SEARCH ||
+                        actionId == EditorInfo.IME_ACTION_GO ||
+                        actionId == EditorInfo.IME_ACTION_NEXT ||
+                        actionId == EditorInfo.IME_ACTION_SEND ||
+                        actionId == EditorInfo.IME_ACTION_DONE ||
+                        (event != null && event.getAction() == KeyEvent.ACTION_DOWN && event.getKeyCode() == KeyEvent.KEYCODE_ENTER)) {
+                    openLocation();
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        location.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if (event.getAction() == KeyEvent.ACTION_DOWN && keyCode == KeyEvent.KEYCODE_ENTER) {
+                    // Perform action on key press
+                    openLocation();
+                    return true;
+                }
+                return false;
+            }
+        });
+    }
+
+    public void openLocation() {
+        String url = location.getText().toString();
+
+        if (url.startsWith("http://")) {
+            // that's cool
+        } else if (url.startsWith("https://")) {
+            // that's cool too
+        } else {
+            url = String.format("http://%s", url);
+        }
+
+        webView.loadUrl(url);
+        location.setSelected(false);
+
+        // hide the soft keyboard
+        InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(location.getWindowToken(), 0);
+        webView.requestFocus();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
